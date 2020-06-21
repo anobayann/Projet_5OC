1549e4148fed7da9703ee3cf7d4d38cc208e34d5
tarridec yann <yann.tarridec@gmail.com>
2020-06-22 05:13:42 +0800

    save before remove input

--- panier.js
+++ panier.js
@@ -27,7 +27,7 @@ function makeRow() {
       quantityOfTeddy.appendChild(document.createElement('input'))
       quantityOfTeddy.classList.add('text-center')
       const inputQuantity = document.querySelector('#tableMother input')
-      inputQuantity.classList.add( 'col-9', 'col-lg-2', 'text-center')
+      inputQuantity.classList.add( 'input','col-9', 'col-lg-2', 'text-center')
       inputQuantity.setAttribute('value', 1) 
       
       const priceOfTeddy = quantityOfTeddy.nextElementSibling
@@ -43,54 +43,59 @@ function addToCart(){
   const cart = JSON.parse(localStorage.getItem('cart')) // on recupere notre panier dans local storage
    
     for (let item in cart) { 
+      
         const eachName = document.querySelectorAll(".cart_name")
-        const eachPrice = document.querySelectorAll(".priceOfTeddy")
+        let eachPrice = document.querySelectorAll(".priceOfTeddy")
+        const eachQuantity = document.querySelectorAll(".input")
+        //const eachQuantity = document.querySelectorAll('.input')
         const lastRow = document.querySelector("#tableMother").lastChild
         const clone = lastRow.cloneNode(true)
 
         document.querySelector("#tableMother").appendChild(clone) // on clone notre row
         eachName[item].innerHTML = JSON.parse(cart[item].name) // on y ajoute le nom
-        eachPrice[item].innerHTML = JSON.parse(cart[item].price) + ' pesos' // on y ajoute le prix
+        
+        eachPrice[item].innerHTML = JSON.parse((cart[item].price)*(cart[item].quantity)) + ' pesos' // on y ajoute le prix
+       // eachQuantity[item].innerHTML = JSON.parse(cart[item].quantity)
+
     }
+            
   tableMother.removeChild(tableMother.lastChild) //on supprime le premier row vide cree
 }
 addToCart();
 
-function teddyPrice(){
 
-  
-
-}
 // Prix total
-function totalPrice(){
+ function totalPrice(){
 
-  const cart = JSON.parse(localStorage.getItem('cart'))
-  let totalPrice = document.querySelector('#totalPrice')
-  let eachPrice = document.querySelectorAll(".priceOfTeddy")
-  let sum = 0
+   const cart = JSON.parse(localStorage.getItem('cart'))
+   let totalPrice = document.querySelector('#totalPrice')
+   let eachPrice = document.querySelectorAll(".priceOfTeddy")
+   
 
-    for (let i in cart){
-      // somme sans prendre en compte les quantites
-      let allPrice = JSON.parse(cart[i].price)
-      sum += allPrice
-      totalPrice.innerHTML = "Prix total = " + sum + " pesos"
+    
+   for (let item in cart) {
  
-      //multiplie les quantites et le prix unitaire
-          
-      // const teddyQuantity = document.querySelectorAll('.custom-select')
-      // teddyQuantity[i].addEventListener("change", function(e) {
+    let sum = 0
+       input = document.querySelectorAll('.input')
+       input[item].addEventListener('input', function()
+       {
         
+         //const addTeddy = JSON.parse(localStorage['cart'])
+         let cartObj = localStorage.getItem('cart')
+         let cart = JSON.parse(cartObj)
+         cart[item].quantity = input[item].value
         
-      //   let newPrice = JSON.parse(e.target.value)*(JSON.parse(cart[i].price))
-      //       eachPrice[i].innerHTML = newPrice + ' pesos'  
-            
-         // })         
-        //comment additionner les nouvelles quantites??? eachPrice retourne nodeliste(object)
-          console.log(JSON.stringify(eachPrice));
-          
-    }
+        localStorage.setItem('cart', JSON.stringify(cart))
+
+        eachPrice[item].innerHTML = (cart[item].price)*(cart[item].quantity) + ' pesos'
+        let allPrice = (cart[item].price)*(cart[item].quantity)
+        
+        sum += allPrice
+        totalPrice.innerHTML = "Prix total = " + sum + " pesos"
+        
+   });
+           }}
   
-}
 
 totalPrice();
 
@@ -98,7 +103,7 @@ totalPrice();
 
 // Vider la pannier
 function clearCart(){
-    const cart = JSON.parse(localStorage.getItem('cart'))
+   // const cart = JSON.parse(localStorage.getItem('cart'))
     const clear_cart = document.querySelector('#clear_button')
     
     clear_cart.addEventListener("click", function(){
