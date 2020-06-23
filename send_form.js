4d8c74962434a5a0a147e10e61e347a6de3c2b63
tarridec yann <yann.tarridec@gmail.com>
2020-06-24 02:30:32 +0800

    tout fonction

--- send_form.js
+++ send_form.js
@@ -1,7 +1,3 @@
-//confirmOnClick(){ 
-
-  
-
   const confirmButton = document.querySelector('#confirm')
   const firstName = document.querySelector('#first_name')
   const firstNameMissing = document.querySelector('#first_name_missing')
@@ -13,70 +9,73 @@
   const cityMissing = document.querySelector('#city_missing')  
   const email = document.querySelector('#email')
   const emailMissing = document.querySelector('#email_missing')
-  const emailRegexe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   //http://emailregex.com/
   const cart = JSON.parse(localStorage.getItem('cart'))
   let products = []
     for (let i in cart) {
       products.push(JSON.parse(cart[i]._id))}
+
   confirmButton.addEventListener("click", form_valid)
   
 
   function form_valid(e){
+    e.preventDefault();
+
     if (products.length == 0){
-      e.preventDefault();
+     
       alert("Oups, vous avez oublié d'adopter un ourson")
     }
     else if (firstName.validity.valueMissing){
-      e.preventDefault();
+     
       firstNameMissing.textContent = "Prenom manquant"
       firstNameMissing.style.color = "red";
     }
     else if(firstName.value.length < 2 || firstName.value.length > 20){
-      e.preventDefault();
+     
       firstNameMissing.textContent = "Le prenom doit contenir 2 lettres minimum et 20 lettres maximum"
       firstNameMissing.style.color = "darkorange";
     
     }
     else if (lastName.validity.valueMissing){
-      e.preventDefault();
+     
       lastNameMissing.textContent = "Nom manquant"
       lastNameMissing.style.color = "red";
     }
     else if(lastName.value.length < 2 || lastName.value.length > 25){
-      e.preventDefault();
+     
       lastNameMissing.textContent = "Le nom doit contenir 2 lettres minimum et 20 lettres maximum"
       lastNameMissing.style.color = "darkorange";
     
     }
     else if (address.validity.valueMissing){
-      e.preventDefault();
+     
       addressMissing.textContent = "Adresse manquante"
       addressMissing.style.color = "red";
     }
     else if(address.value.length < 6 ){
-      e.preventDefault();
+     
       addressMissing.textContent = "L'adresse doit contenir au moins 6 characteres"
       addressMissing.style.color = "darkorange";
     
     }
     else if (city.validity.valueMissing){
-      e.preventDefault();
+     
       cityMissing.textContent = "Ville manquante"
       cityMissing.style.color = "red";
     }
     else if(city.value.length < 2 || city.value.length > 35){
-      e.preventDefault();
+     
       cityMissing.textContent = "Le nom de la ville doit contenir 2 lettres minimum et 35 lettres maximum"
       cityMissing.style.color = "darkorange";
     }
     else if (email.validity.valueMissing){
-      e.preventDefault();
+     
       emailMissing.textContent = "Adresse email manquante"
       emailMissing.style.color = "red";
     }
-    else if(emailRegexe.test(email.value)== false){
-      e.preventDefault();
+    else if(emailRegex.test(email.value)== false){
+     
       emailMissing.textContent = "Format incorrect"
       emailMissing.style.color = "darkorange";
     }
@@ -84,26 +83,18 @@
       sendToServer()
     }
   }
-   
-  
-
-//  confirmOnClick()
-
-
-
-
 
 //Fonction pour confirmer l'achat
 
   function sendToServer(){
   
     const headers = {'Content-Type': 'application/json'} 
-    const firstName = document.getElementById('first_name').value
-    const lastName = document.getElementById('last_name').value
-    const address = document.getElementById('address').value
-    const city = document.getElementById('city').value
-    const email = document.getElementById('email').value
-    const contact = {'firstName': firstName, 'lastName': lastName, 'address': address, 'city': city, 'email': email}
+    const firstNameValue = document.getElementById('first_name').value
+    const lastNameValue = document.getElementById('last_name').value
+    const addressValue = document.getElementById('address').value
+    const cityValue = document.getElementById('city').value
+    const emailValue = document.getElementById('email').value
+    const contact = {'firstName': firstNameValue, 'lastName': lastNameValue, 'address': addressValue, 'city': cityValue, 'email': emailValue}
     const cart = JSON.parse(localStorage.getItem('cart'))
     let products = []
     for (let i in cart) {
@@ -118,23 +109,42 @@
         products: products,
        })
     })
-        .then(function (response) {
-            return response.json();
-        })
-            .then(function (result) {
+        .then(res =>{
+      
+            res.json().then(result => {
                 if (result.orderId) {
                  
-                   window.location.href = 'confirmation.html?_=' + result.orderId
-                   
+                   window.location.href = 'confirmation.html?_=' + result.orderId 
                  }
                 else {
-                  alert("Veillez remplir tout les champs svp")
+                  throw "Oops on arrive pas a recuperer l'Id final?"
+                  
                  }
               })
-        .catch (function (error) {
-              console.log('Request failed', error);
+        .catch (err => {
+          console.error(err)
+          alert("Veillez remplir tout les champs svp")
         })
-}
+})
+  }
+
+/*.then(function (response) {
+  return response.json();
+})
+  .then(function (result) {
+      if (result.orderId) {
+       
+         window.location.href = 'confirmation.html?_=' + result.orderId
+         
+       }
+      else {
+        alert("Veillez remplir tout les champs svp")
+       }
+    })
+.catch (function (error) {
+    console.log('Request failed', error);
+})
+}*/
 
         
  
